553c7dbe80272f0345f8516d8558d63ff71adfeb
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-03-19 02:07:33 -0300

    add: routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,21 @@
 import { Router } from "express";
-import { GetUser } from "../controller/User/index"
+import { GetUser, CreateUser, GetUserById, RemoveUser, UpdateProfileUser } from "../controller/User/index";
+import { CreateProduct, DeleteProduct, GetAllProducts, GetProductById, UpdateProduct } from "../controller/products/index";
+
 const router = Router();
 
-router.get('/', GetUser);
+// Rotas de Usuário
+router.get('/users', GetUser);
+router.post('/users', CreateUser);
+router.get('/users/:id', GetUserById);
+router.delete('/users/:id', RemoveUser);
+router.put('/users/:id', UpdateProfileUser);
+
+// Rotas de Produtos
+router.get('/products', GetAllProducts);
+router.post('/products', CreateProduct);
+router.get('/products/:id', GetProductById);
+router.delete('/products/:id', DeleteProduct);
+router.put('/products/:id', UpdateProduct);
+
+export default router;
