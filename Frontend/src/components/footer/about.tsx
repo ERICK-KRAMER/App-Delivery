85cf9e3ce00c5fc31f3450079a1363c72cc199ff
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-04-11 17:09:52 -0300

    fix: align item

--- Frontend/src/components/footer/about.tsx
+++ Frontend/src/components/footer/about.tsx
@@ -9,7 +9,7 @@ export const About = ({data}: IData) => {
   return (
     <div className="flex flex-col items-center">
       <img src={data.url} alt={data.name} />
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center text-center">
       <h3>Informações de Contato</h3>
       <p>Rua Dois, Centro do Recife/PE</p>
       <p>(81)90000-0000</p>
